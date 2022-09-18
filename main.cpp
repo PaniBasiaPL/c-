be15b48b7dafe93c7b077aa87d5495c17bd29919
PaniBasiaPL <93409065+PaniBasiaPL@users.noreply.github.com>
2022-09-18 22:51:27 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -12,11 +12,44 @@ int main() {
     cin >> a;
     cout << "podaj bok b ";
     cin >> b;
-    int digits = to_string(a*b).length();
-    string spaces = "                                ";
-    for(int i = 0; i < digits; i++) {
-        spaces.erase(spaces.length() - 1);
+    int field = a * b;
+    int perimeter = 2 * a + 2 * b;
+    std::string stars(a, '*');
+    std::string spaces(a - 2, ' ');
+    std::string fieldSpaces;
+    std::string perimeterSpaces;
+    if(a > 12) {
+        fieldSpaces = std::string(a - 9 - to_string(field).length(), ' ');
+        perimeterSpaces = std::string(a - 10 - to_string(perimeter).length(), ' ');
     }
-    spaces += "*";
-    cout << "***********************************" << endl << "*" << " " << a * b << spaces << endl << "*                                 *" << endl << "*                                 *" << endl << "*                                 *" << endl << "***********************************";
+    else {
+        fieldSpaces = std::string(a + to_string(field).length(), ' ');
+        perimeterSpaces = std::string(a + to_string(perimeter).length(), ' ');
+    }
+    std::string cleanLine = "*";
+    for(int i = 0; i < a - 2; i++) {
+        cleanLine += " ";
+    }
+    cleanLine += "*";
+    cout << fieldSpaces.length() << ";;;" << stars.length() << endl;
+    cout << stars << endl;
+    if(fieldSpaces.length() <= stars.length()) {
+        cout << "* pole: " << field << fieldSpaces << "*" << endl;
+    }
+    else {
+        cout << cleanLine << " pole: " << field << endl;
+    }
+    if(perimeterSpaces.length() <= stars.length()) {
+        cout << "* obwod: " << perimeter << perimeterSpaces << "*" << endl;   
+    }
+    else {
+        cout << cleanLine << " obwod: " << perimeter << endl;
+    }
+    std::string rest = "";
+    for(int i = 0; i < b - 4; i++) {
+        rest += cleanLine + "\n";
+    }
+    cout << rest;
+    std::string(a, '*');
+    cout << stars << endl;
 }
